d0656933de688df9f276cd53abd0188bbba4e523
Fenriz1349 <julien.cotte@live.fr>
2025-11-07 08:28:13 +0100

    add TitleView file

---
+++ TravelVista/TravelVista/Views/TitleView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleView.swift
+//  TravelVista
+//
+//  Created by Julien Cotte on 07/11/2025.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleView()
+}
