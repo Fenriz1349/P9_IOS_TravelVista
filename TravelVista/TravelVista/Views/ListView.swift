6bb0070283965c172030328057cb006a2fd86f00
Fenriz1349 <julien.cotte@live.fr>
2025-11-12 16:31:19 +0100

    add navigation from loadingScreen to ListView

--- TravelVista/TravelVista/Views/ListView.swift
+++ TravelVista/TravelVista/Views/ListView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct ListView: View {
     let regions: [Region] = Service().load("Source.json")
+    private let viewModel = ListViewModel()
 
     var body: some View {
         List {
             ForEach(regions, id: \.name) { region in
-                Section(
-                    header: Text(region.name)
-                        .foregroundColor(.gray)
-                ) {
+                Section(header: Text(region.name)
+                    .foregroundColor(.gray)) {
                     ForEach(region.countries, id: \.name) { country in
-                        CountryRow(country: country)
+                        Button {
+                            viewModel.openDetailView(for: country)
+                        } label: {
+                            CountryRow(country: country)
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
             }
         }
         .listStyle(.plain)
+        .navigationTitle("Liste de voyages")
     }
 }
 
